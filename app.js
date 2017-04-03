20b968290446676564c72cc767a413712f2eb424
Maks <pvl1720@gmail.com>
2017-04-03 01:51:05 -0700

    added a basic conctructor function

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ function randomNum(min, max){
 }
 
 document.querySelector('.randomNum').innerHTML = randomNum(1, 100);
+
+function Student(name, course, school, location) {
+  this.firstName = name;
+  this.course = course;
+  this.school = school;
+  this.location = location;
+}
+var Maks = new Student('Maks', '301 Intermediate Software Development', 'Code Fellow', 'Seattle, WA.');
+
+document.getElementById('summary').innerHTML =
+'My name is ' + Maks.firstName + '. I\'m currently taking ' + Maks.course + ' at ' + Maks.school + ', located in ' + Maks.location;
