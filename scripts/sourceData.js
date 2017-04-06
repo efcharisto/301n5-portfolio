d9cdb10d837aa6acca3c026fb85b275ebfc1699e
Maks <pvl1720@gmail.com>
2017-04-05 20:12:44 -0700

    added description for jobsObject and some properties

--- scripts/sourceData.js
+++ scripts/sourceData.js
@@ -1,17 +1,22 @@
 'use strict';
 
-var = firstObject [
+var jobsObject = [
   {
-    name:  'Project 1',
-    body: 'this is some text in body about Project 1'
+    name:  'Job 1',
+    body: 'this is some text in body about Job 1',
+    time: 'I had this job for took me 2 years.',
+    purpose: 'Manage and develop a team of passionate individuals in order to exceed sales goals and meet required budget metrics'
   },
   {
-    name:  'Project 2',
-    body: 'this is some text in body about Project 2'
+    name:  'Job 2',
+    body: 'this is some text in body about Job 2',
+    time: 'I had this job for took me 2 years.',
+    purpose: 'To fundraise for a local non-profit in order to maintain flow of finances to fund scholarships. Engage wiht the Alumni network through organizing events, mixers, and workshops'
   },
   {
-    name:  'Project 3',
-    body: 'this is some text in body about Project 3'
+    name:  'Job 3',
+    body: 'this is some text in body about Job 3',
+    time: 'I had this job for took me 6 months.',
+    purpose: 'Assisting internal users with hardware and software related issues and performing server administration tasks in Microsoft AD environment'
   },
-
 ];
