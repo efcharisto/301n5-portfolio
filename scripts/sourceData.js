20bf613c6f7512fedadf561e5ccf855b8769a3a2
Maks <pvl1720@gmail.com>
2017-04-05 19:55:47 -0700

    SECOND LAB. organized files better and started working on my sourceData object in a separate JS file

---
+++ scripts/sourceData.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var = firstObject [
+  {
+    name:  'Project 1',
+    body: 'this is some text in body about Project 1'
+  },
+  {
+    name:  'Project 2',
+    body: 'this is some text in body about Project 2'
+  },
+  {
+    name:  'Project 3',
+    body: 'this is some text in body about Project 3'
+  },
+
+];
