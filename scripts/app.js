2a7f259856c17103fce3ad2d61cbc155134d87b9
Maks <pvl1720@gmail.com>
2017-04-12 20:24:04 -0700

    added files from lab 1 class to get the icons in there to add the hamburger but still need to make it work

--- scripts/app.js
+++ scripts/app.js
@@ -12,7 +12,26 @@ function Student(name, course, school, location) {
   this.school = school;
   this.location = location;
 }
+
 var Maks = new Student('Maks', '301 Intermediate Software Development', 'Code Fellow', 'Seattle, WA.');
 
 document.getElementById('summary').innerHTML =
 'My name is ' + Maks.firstName + '. I\'m currently taking ' + Maks.course + ' at ' + Maks.school + ', located in ' + Maks.location;
+
+function Jobs (jobsObject) {
+  this.name = jobsObject.name;
+  this.body = jobsObject.body;
+  this.time = jobsObject.time;
+  this.purpose = jobsObject.purpose;
+}
+
+Jobs.prototype.toHtml = function() {
+  var $postJobs = $('jobs.template').clone();
+  $postJobs.find('div').html(this.name);
+  $postJobs.find('div').html(this.body);
+  $postJobs.find('div').html(this.time);
+  $postJobs.find('div').html(this.purpose);
+
+  return $postJobs;
+
+}
