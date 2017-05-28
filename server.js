dfe81bc49d82774238752bb6bf53751d786f6fb6
Maks <pvl1720@gmail.com>
2017-05-28 00:43:50 -0700

    localhost 300 running

--- server.js
+++ server.js
@@ -12,3 +12,5 @@ app.get('*', function(req, res){
 app.listen(PORT, function(){
   console.log('Server is running on port: ' + PORT);
 });
+
+// installed express and now the server now runs the page on localhost:3000
