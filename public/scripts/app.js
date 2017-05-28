848c8696d9fbf3c3f1606b1ac94694be12f78acc
Maks <pvl1720@gmail.com>
2017-05-27 23:56:52 -0700

    basic server.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -55,12 +55,18 @@ document.getElementById('summary').innerHTML =
 // ******************
 $(() => {
   $.ajax({
-    url: '/data/jobs.json'
+    url: 'data/jobs.json'
   }).done(function(data) {
-    console.log('request done: ' + Date.now());
     data.forEach((note) => {
       $('#notes').append(`<li>${note.body}</li>`);
     });
   });
-  console.log(Date.now());
+  console.log('request started: ' + Date.now());
+});
+
+
+$(document).ready(function(){
+    $('.randomNum').click(function(){
+        $('p').append(`data`);
+    });
 });
